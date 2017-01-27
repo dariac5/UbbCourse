cc851fc53165667cdf6a382ef746a20052f44af8
dariac5 <darya_c5@yahoo.com>
2017-01-27 21:33:27 +0200

    UbbCourse1

---
+++ UbbCourse1/Mercedes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UbbCourse1
+{
+    public class Mercedes : AbstractCar
+
+    {
+        private string _description;
+        private int _price;
+
+        public string Description
+        {
+            get { return Description;}
+            internal set { Description = value; }
+        }
+
+        public int Price
+        {
+            get { return Price; }
+            internal set { Price = value; }
+        }
+
+        public Mercedes() 
+        {
+            _description = "E Class";
+            _price = 67000;
+        }   
+
+    }
+
+    }
---
+++ UbbCourse1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UbbCourse1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mercedes mercedes = new Mercedes();
+            EngineStruct engine = new EngineStruct();
+            ICar car = new Mercedes();
+            AbstractCar car2 = new Mercedes();
+            Console.WriteLine("Mercedes is " + mercedes.Description);
+            Console.WriteLine("Mercedes costs " + mercedes.Price);
+            Console.ReadKey();
+        }
+    }
+
+    public struct EngineStruct 
+    {
+    }
+}
